8d75c01f81b0721778ecf0d04755be0623369ddb
Em Shoder <ladrenrights@gmail.com>
2022-06-03 15:27:31 -0700

    Added array list class

---
+++ DataStructs/src/LinearStructures/List.java
@@ -0,0 +1,24 @@
+package LinearStructures;
+
+/**
+ * Interface that defines general functions that a list should
+ * support
+ *
+ * @param <T>
+ */
+public interface List<T>
+{
+	public T get(int index);
+	
+	public void append(T item);
+	
+	public void insert(int index, T item);
+	
+	public void clear();
+	
+	public T remove(int index);
+	
+	public int size();
+	
+	public boolean isEmpty();
+}
