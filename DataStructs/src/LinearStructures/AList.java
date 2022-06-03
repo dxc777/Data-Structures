dd45a0393009cb421da67c8625f4204524bb9062
Em Shoder <ladrenrights@gmail.com>
2022-06-03 15:33:45 -0700

    Added AList constructors

--- DataStructs/src/LinearStructures/AList.java
+++ DataStructs/src/LinearStructures/AList.java
@@ -1,15 +1,43 @@
 package LinearStructures;
 
+import java.util.Arrays;
+
 public class AList<T> implements List<T>
 {	
 	private T[] arr;
 	
 	private int size;
 
+	public AList() 
+	{
+		this(10);
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public AList(int size) 
+	{
+		arr = (T[]) new Object[size]; 
+		size = 0;
+	}
+	
+	public AList(AList<T> obj) 
+	{
+		this.arr = Arrays.copyOf(obj.arr, obj.size);
+		this.size = obj.size;
+	}
+	
+	
+	/**
+	 * Return element at index passed in
+	 * Valid indexes are from 0 -> size - 1 if it is empty there is no valid index
+	 */
 	@Override
 	public T get(int index)
 	{
-		return null;
+		if(index < 0 || index >= size) 
+			throw new IndexOutOfBoundsException();
+		return arr[index];
 	}
 
 	@Override
