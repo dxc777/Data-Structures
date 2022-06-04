13cbcd47077e714e4bbe43c9ec9e15e1f110d343
Em Shoder <ladrenrights@gmail.com>
2022-06-03 17:58:07 -0700

    Added constructors

--- DataStructs/src/LinearStructures/Link.java
+++ DataStructs/src/LinearStructures/Link.java
@@ -3,8 +3,16 @@ package LinearStructures;
 public class Link<T>
 {
 	public T element;
+	
 	public Link<T> next;
 	
+	public Link() {}
+	
+	public Link(T element, Link<T> next) 
+	{
+		this.element = element;
+		this.next = next;
+	}
 	
 	public String toString() 
 	{
