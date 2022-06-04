57a9ae5a398bd782b4345916a3735ae13385ddc7
Em Shoder <ladrenrights@gmail.com>
2022-06-03 17:51:33 -0700

    Changed list.java to abstractlist.java to distinguish it from java.util.list

---
+++ DataStructs/src/LinearStructures/LList.java
@@ -0,0 +1,55 @@
+package LinearStructures;
+
+public class LList<T> implements List<T>
+{
+
+	@Override
+	public T get(int index)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void append(T item)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insert(int index, T item)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T remove(int index)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
